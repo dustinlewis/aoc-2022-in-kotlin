c5dc95005d039a8950b1960b25203dc06357dba5
Dustin Lewis <dustin.lewis@jobnimbus.com>
2022-12-03 12:43:54 -0700

    Clean up day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,45 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val elfs = mutableListOf<MutableList<Int>>()
-        elfs.add(mutableListOf())
-        input.forEach() {
+        val elves = mutableListOf<MutableList<Int>>()
+        elves.add(mutableListOf())
+        input.forEach {
             if (it.isEmpty()) {
-                elfs.add(mutableListOf())
+                elves.add(mutableListOf())
             }
             else {
-                elfs[elfs.lastIndex].add(it.toInt())
+                elves[elves.lastIndex].add(it.toInt())
             }
         }
-        val totals = elfs.map { it.sum() }
-
-//        val elfs = input.fold(mutableListOf<MutableList<String>>()) {
-//            acc, el ->
-//            {
-//                if (el.isEmpty()) acc.add(mutableListOf()) else acc[acc.lastIndex].add(el)
-//                acc
-//            }
-//        }
-        return totals.max()
+        return elves.maxOfOrNull { it.sum() } ?: 0
     }
 
     fun part2(input: List<String>): Int {
-        val elfs = mutableListOf<MutableList<Int>>()
-        elfs.add(mutableListOf())
-        input.forEach() {
+        val elves = mutableListOf<MutableList<Int>>()
+        elves.add(mutableListOf())
+        input.forEach {
             if (it.isEmpty()) {
-                elfs.add(mutableListOf())
+                elves.add(mutableListOf())
             }
             else {
-                elfs[elfs.lastIndex].add(it.toInt())
+                elves[elves.lastIndex].add(it.toInt())
             }
         }
-        val totals = elfs.map { it.sum() }
-        return totals.sortedDescending().take(3).sum()
+        return elves.map { it.sum() }.sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
